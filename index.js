678986aa00399d15a7bcd03f0402e714fedb20a3
Agustin Arce <agustinarceprincich@gmail.com>
2024-04-26 21:11:11 +0000

    Endpoint implemented

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const mimeTypes = require('mime-types');
 require('dotenv').config()
-
 var app = express();
 
 app.use(cors());
@@ -11,8 +12,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
+app.post('/api/fileanalyse', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
 
+  const fileName = req.file.originalname;
+  const fileType = mimeTypes.lookup(fileName);
+  const fileSize = req.file.size;
+
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
